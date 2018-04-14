9f693aaf9a8ad3c5a0e82b4508aa28e22ba057d8
AlexShan2008 <happyshantong@163.com>
2018-04-14 11:36:42 +0800

    http over

---
+++ 23.static/README.md
@@ -0,0 +1,20 @@
+## 创建一个静态服务器  发布到npm上面 
+
+> npm link
+> http-server -p 3000 
+> --help
+
+> yargs 包 用来处理命令行的参数 process.argv 
+```
+yarn add yargs 
+
+> 处理  
+#!
+> 注意，项目中所有的依赖都是上线依赖；@1.0.1
+
+> 发包到Npm 
+> 1. npm 上没有相关的包；
+> 2. 切换源到npm源，非cnpm
+> 3. npm publish 发布到线上
+> 4. npm install sg-server -g
+> 4. npm update sg-server
--- 23.static/src/app.js
+++ 23.static/src/app.js
@@ -9,6 +9,12 @@ const url = require('url');
 const supervisor = require('supervisor');//类似nodemon 监听文件的更改，更改后不用重启服务；
 const debug = require('debug')('static:app');//后面放参数；可以跟后面的参数决定是否打印;环境变量
 const stat = util.promisify(fs.stat);
+const ejs = require('ejs');
+const tmpl = fs.readdirSync('')
+
+// ejs 模板
+// <% dirs.forEach(dir=>{ %> js语句用此语法
+// <%= dir.path %>"> 取值
 
 // 设置环境变量
 // set DEBUG=static:app 
@@ -25,20 +31,23 @@ class Server {
     }
     handleRequest() {
         return async (req, res) => {
-            let {pathname} = url.parse(req.url,true);
-            try{
-            let statObj =  await stat(p);
-            // 是目录，展示内容，否则返回文件
-            if(statObj.isDirectory()){
+            let { pathname } = url.parse(req.url, true);
+            try {
+                let statObj = await stat(p);
+                // 是目录，展示内容，否则返回文件
+                if (statObj.isDirectory()) {
+                    // 是目录函数目录内容
+                    // 模板引擎 ejs 
+                    ejs.render(this.tmpl, { dirs: [{ path: 'abddcd', name: 'xxx' }] })
 
-            }else{
-                // 压缩
-                // 缓存
-                // 断点续传
-                
-            }
+                } else {
+                    // 压缩
+                    // 缓存
+                    // 断点续传
 
-            }catch(e){
+                }
+
+            } catch (e) {
                 res.statusCode = 404;
                 res.end();
 
@@ -50,11 +59,10 @@ class Server {
         let { port, hostname } = this.config;
         let server = http.createServer(this.handleRequest());
         let url = `http://${hostname}:${chalk.green(port)}`;
-        debug(url+'hello');
-        server.listen(port,hostname);
+        debug(url + 'hello');
+        server.listen(port, hostname);
     }
 
 }
 
-let server = new Server();
-server.start();
+module.exports = Server;
